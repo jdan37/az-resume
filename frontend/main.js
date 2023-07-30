10770290b81969f6548dc2f96eca785b77bec8a5
jdan38 <35674798+jdan38@users.noreply.github.com>
2023-07-30 19:39:48 -0400

    adding content to pages continued

--- frontend/main.js
+++ frontend/main.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
-const functionApiUrl = 'http://getazresumecounter.azurewebsites.net/api/GetResumeCounter';
+const functionApiUrl = 'https://getazresumecounter.azurewebsites.net/api/GetResumeCounter';
 const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
